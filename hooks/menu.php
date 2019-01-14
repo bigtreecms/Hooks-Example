9b9398d47ef7eadc46f8a5a4ebf3cd1ea8f1b9b5
Tim Buckingham <mr@timbuckingham.com>
2019-01-14 14:59:54 -0500

    Fixing navigation state of menu hook, adding comments

--- hooks/menu.php
+++ hooks/menu.php
@@ -1,5 +1,7 @@
 <?php
 	$data["dashboard"]["children"][] = [
 		"title" => "BigTreeCMS.org",
-		"link" => "http://www.bigtreecms.org"
+		"link" => "http://www.bigtreecms.org",
+		"hidden" => true, // Don't draw this in sub-navigation on the dashboard
+		"top_level_hidden" => false // Draw this in the drop-down navigation
 	];
